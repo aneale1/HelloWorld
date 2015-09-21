18f1cf03cb31f07214cdb0443420c274483415b1
Arpad Neale <aneale1@jhu.edu>
2015-09-21 19:05:30 -0400

    Repeat n times

---
+++ hello-world.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+	int i = 0; //interator
+	int n = 5;
+	//repeat n times
+	for(i=0; i<n; i++) {
+	printf("Hello World!\n");
+
+	//printf("argc = %d\n", argc);
+	}
+
+	return EXIT_SUCCESS;
+
+
+
+}
+
